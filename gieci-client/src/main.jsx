46ffdfa3a7f73f6f58bb434ace3c0bcb5311e3ef
Correia <teteuline152@gmail.com>
2026-09-25 05:47:41 -0300

    Update main.jsx

--- gieci-client/src/main.jsx
+++ gieci-client/src/main.jsx
@@ -1,22 +1,52 @@
 import { createRoot } from "react-dom/client";
+
 import App from "./App.jsx";
+
 import SessionGate from "./components/SessionGate.jsx";
+
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
+
+
 import "@fontsource/space-grotesk/latin-500.css";
 import "@fontsource/space-grotesk/latin-600.css";
 import "@fontsource/space-grotesk/latin-700.css";
+
 import "@fontsource/ibm-plex-sans/latin-400.css";
 import "@fontsource/ibm-plex-sans/latin-500.css";
 import "@fontsource/ibm-plex-sans/latin-600.css";
+
 import "@fontsource/ibm-plex-mono/latin-500.css";
 import "@fontsource/ibm-plex-mono/latin-600.css";
 import "@fontsource/ibm-plex-mono/latin-700.css";
+
+
 import "./styles/original.css";
+
 import "./styles/extracted.css";
+
 import "./styles/responsive.css";
 
-createRoot(document.getElementById("root")).render(
+/*
+  IMPORTANTE:
+  deixe shelves.css por último,
+  porque ele organiza os cards sem
+  destruir o restante do visual.
+*/
+import "./styles/shelves.css";
+
+
+createRoot(
+  document.getElementById("root")
+).render(
+
   <ErrorBoundary>
-    <SessionGate>{(props) => <App {...props} />}</SessionGate>
-  </ErrorBoundary>,
+
+    <SessionGate>
+      {(props) => (
+        <App {...props} />
+      )}
+    </SessionGate>
+
+  </ErrorBoundary>
+
 );
